29b833b5eb4946aed2c2b99a1e2762740672ea00
Luciano Abraham <lucianoabrahammoise27@gmail.com>
2023-01-03 17:50:17 -0300

    Se creo carpeta assets y se agrego las imagenes desde carpeta

--- src/components/GridItems/GridItems.jsx
+++ src/components/GridItems/GridItems.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom'
 import { AboutSection } from '../AboutSection/AboutSection'
-
+const imgHome = require.context('../../assets/img/homeImg',true)
 
 export const GridItems = () => {
 
@@ -9,10 +9,10 @@ export const GridItems = () => {
     <>
     
         <div className="grid-item tall" >
-                    <img src="http://quintotiempo.com/media/k2/items/cache/2fa67f482133f1c934235b73c2a03954_XL.jpg" alt="" />
+                    <img src={imgHome(`./img-home-1.jpg`)}alt="" />
                 </div>
                 <div className="grid-item">
-                    <img src="https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/06/28182001/Mendozaa.jpg" alt="" />
+                    <img src={imgHome(`./img-home-1.jpg`)} alt="" />
                 </div>
                 <div className="grid-item links-item">
                     <NavLink href="/carta" to='carta' className='link-carta-container'>
@@ -22,7 +22,7 @@ export const GridItems = () => {
                     </NavLink>
                 </div>
                 <div className="grid-item">
-                    <img src="https://oleico.com/wp-content/uploads/2020/09/post_thumbnail-b6d279093b7698f952dd017059e794df.jpeg" alt="" />
+                    <img src={imgHome(`./img-home-3.jpeg`)} alt="" />
                 </div>
 
                 <div className="grid-item pedir-item links-item">
@@ -33,13 +33,13 @@ export const GridItems = () => {
                 </NavLink>
                 </div>
                 <div className="grid-item wide">
-                    <img src="https://cubiro.com/wp-content/uploads/2019/07/comidas-de-lujo-que-puedes-preparar-800x445.jpg" alt="" />
+                    <img src={imgHome(`./img-home-4.jpg`)} alt="" />
                 </div>
                 <div className="grid-item ">
-                <img src="https://cdn7.kiwilimon.com/clasificacion/3345/3345.jpg" alt="" />
+                <img src={imgHome(`./img-home-5.jpg`)} alt="" />
                 </div>
                 <div className="grid-item ">
-                <img src="https://heladitos.com/wp-content/uploads/2017/07/ice-2305144_960_720.jpg" alt="" />
+                <img src={imgHome(`./img-home-6.jpg`)} alt="" />
                 </div>
                 <div className="grid-item wide about-main links-item">
                     <AboutSection/>
@@ -52,11 +52,11 @@ export const GridItems = () => {
                 </NavLink>
                 </div>
                 <div className="grid-item wide">
-                    <img src="https://www.caraotadigital.net/wp-content/uploads/2020/10/12536225-807F-4BDE-B15E-7EAB8C630A8E-1-1068x601.jpeg" alt="" />
+                    <img src={imgHome(`./img-home-7.jpeg`)} alt="" />
                     
                 </div>
                 <div className="grid-item">
-                <img src="https://t2.rg.ltmcdn.com/es/posts/3/1/2/carne_al_vino_con_salsa_47213_600_square.jpg" alt="" />
+                <img src={imgHome(`./img-home-8.jpg`)} alt="" />
                 </div>
                 
     </>
